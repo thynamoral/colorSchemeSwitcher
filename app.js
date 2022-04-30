f015abbc9ecf8a7458a0b43347616415cb6a6456
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2022-04-30 13:58:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 
 grey.addEventListener('click',() => {
-    document.body.style.backgroundColor = 'grey';
+    document.body.style.backgroundColor = '#ffafcc';
     h1.style.color = 'white';
     p.style.color = 'white';
 })
